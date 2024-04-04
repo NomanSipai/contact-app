cc3bb9a2a08a5f09d4a09e90832d5e494c7f87f1
nomanvaghela <nomanvaghela7@gmail.com>
2024-04-04 18:46:48 +0530

    ws

--- src/App.jsx
+++ src/App.jsx
@@ -182,7 +182,10 @@ function App() {
               className="w-full border-2 border-gray-400 rounded p-5  bg-slate-50"
               value={newContactData.gender}
               onChange={(e) =>
-                setNewContactData({ ...newContactData, gender: e.target.value })
+                setNewContactData({
+                  ...newContactData,
+                  gender: e.target.value,
+                })
               }>
               <option className="bg-white" value="">
                 Gender
@@ -309,17 +312,17 @@ function App() {
             Contact List
           </div>
 
-          <table className="shadow hidden md:block shadow-gray-600 bg-slate-50 divide-y divide-gray-200">
+          <table className=" w-full shadow shadow-gray-600 bg-slate-50 divide-y divide-gray-200">
             <thead className="bg-gray-50 ">
               <tr>
                 <th
                   scope="col"
-                  className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  className="pl-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                   Name
                 </th>
                 <th
                   scope="col"
-                  className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  className="p-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                   Phone
                 </th>
                 <th
@@ -343,8 +346,8 @@ function App() {
               {contactData.map((item) => {
                 return (
                   <tr key={item.id}>
-                    <td className="px-6 py-4 whitespace-nowrap">{item.name}</td>
-                    <td className="px-6 py-4 whitespace-nowrap">
+                    <td className="pl-6 py-4 whitespace-nowrap">{item.name}</td>
+                    <td className="pr-6 py-4 whitespace-nowrap">
                       {item.phone}
                     </td>
                     <td className="px-6 py-4 whitespace-nowrap">
@@ -383,47 +386,6 @@ function App() {
               })}
             </tbody>
           </table>
-          <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 md:hidden">
-            {contactData.map((item) => {
-              return (
-                <div
-                  key={item.id}
-                  className="bg-white space-y-3 p-4 rounded-lg shadow">
-                  <div className="flex items-center space-x-2 text-sm">
-                    <div>{item.name}</div>
-                  </div>
-                  <div>{item.gender}</div>
-                  <div className="text-sm text-gray-700">{item.phone}</div>
-                  <div className="text-sm font-medium text-black">
-                    <button
-                      className="p-2 me-2 bg-gray-200 rounded"
-                      onClick={() => handleEditContact(item)}>
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="20"
-                        height="20"
-                        className="bi bi-pencil-fill cursor-pointer"
-                        viewBox="0 0 16 16">
-                        <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.5.5 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11z" />
-                      </svg>
-                    </button>
-                    <button
-                      className="p-2 ms-2 rounded bg-[#fbe3e6]"
-                      onClick={() => handleDeleteContact(item)}>
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="20"
-                        height="20"
-                        className="bi bi-trash3-fill fill-[#e03546]  cursor-pointer"
-                        viewBox="0 0 16 16">
-                        <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5m-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5M4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06m6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528M8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5" />
-                      </svg>
-                    </button>
-                  </div>
-                </div>
-              );
-            })}
-          </div>
         </div>
       </div>
     </div>
