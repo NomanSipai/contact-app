49f5a63e1ea19af66ff16deeec103ec32aa8ac6a
nomanvaghela <nomanvaghela7@gmail.com>
2024-04-05 00:17:14 +0530

    error solve

--- src/App.jsx
+++ src/App.jsx
@@ -120,6 +120,13 @@ function App() {
     <div className={`min-h-screen pt-2 bg-[#fbe3e6]`}>
       <div>
         <Toaster />
+        {alertPopUp && (
+          <AlertPopUp
+            popUpItem={popUpItem}
+            handleDeleteContact={handleDeleteContact}
+            setAlertPopUp={setAlertPopUp}
+          />
+        )}
       </div>
       <div className=" bg-[#e03546] p-5  me-2 ms-2 shadow shadow-gray-500 text-white text-xl font-medium">
         Contact App
@@ -350,56 +357,47 @@ function App() {
               <tbody className="bg-white ">
                 {contactData.map((item) => {
                   return (
-                    <>
-                      {alertPopUp && (
-                        <AlertPopUp
-                          popUpItem={popUpItem}
-                          handleDeleteContact={handleDeleteContact}
-                          setAlertPopUp={setAlertPopUp}
-                        />
-                      )}
-                      <tr key={item.id}>
-                        <td className="pl-6 py-4 whitespace-nowrap">
-                          {item.name}
-                        </td>
-                        <td className="pr-6 py-4 whitespace-nowrap">
-                          {item.phone}
-                        </td>
-                        <td className="px-6 py-4 whitespace-nowrap">
-                          {item.gender}
-                        </td>
-                        <td className="px-6 py-4 whitespace-nowrap">
-                          <button
-                            className="p-2 bg-gray-200 rounded"
-                            onClick={() => handleEditContact(item)}>
-                            <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              width="20"
-                              height="20"
-                              className="bi bi-pencil-fill cursor-pointer"
-                              viewBox="0 0 16 16">
-                              <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.5.5 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11z" />
-                            </svg>
-                          </button>
-                        </td>
-                        <td className="px-6 py-4 whitespace-nowrap">
-                          <button
-                            className="p-2 rounded bg-[#fbe3e6]"
-                            onClick={() =>
-                              setAlertPopUp(true) || setPopUpItem(item)
-                            }>
-                            <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              width="20"
-                              height="20"
-                              className="bi bi-trash3-fill fill-[#e03546]  cursor-pointer"
-                              viewBox="0 0 16 16">
-                              <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5m-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5M4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06m6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528M8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5" />
-                            </svg>
-                          </button>
-                        </td>
-                      </tr>
-                    </>
+                    <tr key={item.id}>
+                      <td className="pl-6 py-4 whitespace-nowrap">
+                        {item.name}
+                      </td>
+                      <td className="pr-6 py-4 whitespace-nowrap">
+                        {item.phone}
+                      </td>
+                      <td className="px-6 py-4 whitespace-nowrap">
+                        {item.gender}
+                      </td>
+                      <td className="px-6 py-4 whitespace-nowrap">
+                        <button
+                          className="p-2 bg-gray-200 rounded"
+                          onClick={() => handleEditContact(item)}>
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="20"
+                            height="20"
+                            className="bi bi-pencil-fill cursor-pointer"
+                            viewBox="0 0 16 16">
+                            <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.5.5 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11z" />
+                          </svg>
+                        </button>
+                      </td>
+                      <td className="px-6 py-4 whitespace-nowrap">
+                        <button
+                          className="p-2 rounded bg-[#fbe3e6]"
+                          onClick={() =>
+                            setAlertPopUp(true) || setPopUpItem(item)
+                          }>
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="20"
+                            height="20"
+                            className="bi bi-trash3-fill fill-[#e03546]  cursor-pointer"
+                            viewBox="0 0 16 16">
+                            <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5m-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5M4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06m6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528M8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5" />
+                          </svg>
+                        </button>
+                      </td>
+                    </tr>
                   );
                 })}
               </tbody>
